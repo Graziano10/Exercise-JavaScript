9b6934b52cfd6df07a66bb7b2a281291e4e549e6
Graziano Corineo <grazianocorineo01@gmail.com>
2023-03-09 16:16:03 +0100

    Add: Exercise-JavaScript-05

---
+++ Es-05/exercise-1.js
@@ -0,0 +1,15 @@
+const person1 = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 25,
+};
+
+const person2 = 'Simon Doe 25';
+
+// Modify the property `firstName` of the `person2` in "Simon".
+
+console.log(person1);
+console.log(person2);
+
+
+// L'ogetto n°1 è il template non viene modificato
