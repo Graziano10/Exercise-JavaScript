ccf2075ec15224693346a018056df062a1e8c419
Graziano Corineo <119019545+Graziano10@users.noreply.github.com>
2023-03-11 22:44:34 +0100

    Add: Exercise-JavaScript-09

---
+++ Es-09/exercise-1.js
@@ -0,0 +1,13 @@
+
+
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+const result = Object.keys(person)
+for (let i = 0; i < print.length; i++) {
+  console.log(`${Object.keys(person)[i]}:${Object.values(person)[i]}`)
+};
+
