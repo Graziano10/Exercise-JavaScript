49982fd3a4b00c9a93324f5b22427e394a19862c
Graziano Corineo <119019545+Graziano10@users.noreply.github.com>
2023-03-11 03:14:25 +0100

    Add: Exercise-JavaScript-09

---
+++ Es-09/exercise-1.js
@@ -0,0 +1,11 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+console.log(Object.entries(person));
+
+
+//console.log(Object.keys(person));
+// Con Object.kyes l output è un arrey di kyes (Cosa sto sbagliando?) 
