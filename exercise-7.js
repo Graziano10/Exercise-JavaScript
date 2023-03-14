babab352d28081cbc72c571a8746a2efdbb7e438
Graziano Corineo <grazianocorineo01@gmail.com>
2023-03-15 00:25:28 +0100

    Add: Exercise-JavaScript-19 Getters and Setters

---
+++ exercise-7.js
@@ -0,0 +1,38 @@
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  get firstName() {
+    return this.firstName;
+  }
+
+  set firstName(value) {
+    this.firstName = value;
+  }
+
+  get lastName() {
+    return this.lastName;
+  }
+
+  set lastName(value) {
+    this.lastName = value;
+  }
+
+  get age() {
+    return this.age;
+  }
+
+  set age(value) {
+    this.age = value;
+  }
+}
+
+const person = new Person("Mario", "Rossi", 25);
+console.log(person.fullName);
+
+person.firstName = "Maria";
+person.lastName = "Verdi";
+console.log(person.fullName);
