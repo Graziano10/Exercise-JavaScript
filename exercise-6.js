fdbbef53811434e0ee7932240b07c10536449044
Graziano Corineo <grazianocorineo01@gmail.com>
2023-03-16 16:14:32 +0100

    Add: Exercise-JavaScript-20/instanceof Operator

---
+++ exercise-6.js
@@ -0,0 +1,41 @@
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+}
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+}
+
+class AreaCalculator {
+  static calculateSquare (square) {
+    return square.side * square.side;
+  }
+
+  static calculateRectangle (rectangle) {
+    return rectangle.width * rectangle.height
+  }
+
+  static calculateCircle (circle) {
+    return Math.PI * Math.pow(circle.radius, 2);
+    //return Math.PI * (circle.radius * circle.radius);
+  }
+}
+
+const square = new Square(4);
+const rectangle = new Rectangle(4, 2);
+const circle = new Circle(5);
+
+console.log(AreaCalculator.calculateSquare(square));
+console.log(AreaCalculator.calculateRectangle(rectangle));
+console.log(AreaCalculator.calculateCircle(circle));
