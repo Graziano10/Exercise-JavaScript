74ba2b4872ae151176ab4ee268596400b204a1c9
Graziano Corineo <grazianocorineo01@gmail.com>
2023-03-22 23:58:36 +0100

    Add: Exercise-JavaScript-28/Rest parameters and spread syntax

---
+++ exercise-2.js
@@ -0,0 +1,19 @@
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+};
+
+const id = person.id;
+const personInfo = {
+  firstName: person.firstName,
+  lastName: person.lastName,
+  age: person.age
+};
+
+const {...firstName} = personInfo
+
+console.log(id, firstName);
+
+
