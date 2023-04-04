9e99ea0de789197059d2c6e8932ed8d7fac8e12b
Graziano Corineo <grazianocorineo01@gmail.com>
2023-04-05 01:19:57 +0200

    Add: Exercise-JavaScript-30/JSON Methods

--- exercise-2.js
+++ exercise-2.js
@@ -6,14 +6,16 @@ class Person {
     this.age = age;
   }
 
+  static fromJson(json) {
+    let obj = JSON.parse(json);
+    return new Person(obj.id, obj.firstName, obj.lastName, obj.age)
+  }
+
   toJson() {
     return JSON.stringify(this);
   }
 }
 
 const json = '{"id":1,"firstName":"Mario","lastName":"Rossi","age":25}';
-// const developer = parse.fromJson(json);
-// console.log(developer);
-
-const jsonobj = JSON.parse(json);
-console.log(jsonobj);
+const developer = Person.fromJson(json);
+console.log(developer);
