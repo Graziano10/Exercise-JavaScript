a4730dccd04b6702020fc600442a603733e93834
Graziano Corineo <grazianocorineo01@gmail.com>
2023-04-07 01:14:59 +0200

    Add: Exercise-JavaScript-37/Classes inheritance/ Promise

---
+++ exercise.js
@@ -0,0 +1,17 @@
+
+
+const number = 15;
+
+const verify = async () => {
+    try {
+        if (number > 10) {
+            console.log('The constant is greater than 10 : Resolve')
+        } else {
+            console.log('The constant is not greater than 10 : Reject');
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+verify();
