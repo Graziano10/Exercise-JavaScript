4d590edc3d5ed55891cf353cf54125524186cc81
Graziano Corineo <grazianocorineo01@gmail.com>
2023-04-11 18:40:48 +0200

    Add: Exercise-JavaScript-04 /Conditional and Loops

--- Es-04/exercise-7.js
+++ Es-04/exercise-7.js
@@ -1,5 +1,12 @@
-function sumUntil(a) {
-    return a * 3
-}
+const sumUntil = (maxValue) => {
+    let i;
+    let sum = 0;
 
-console.log(sumUntil(5));
+    for(i = 0; i <= maxValue; i++)
+    {
+      sum = sum + i;
+     }
+     return sum;
+  }
+  
+  console.log(sumUntil(5));
