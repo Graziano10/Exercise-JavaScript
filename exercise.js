aa944114bea8a284741a06cb47425a137fca2ef4
Graziano Corineo <grazianocorineo01@gmail.com>
2023-04-11 12:29:15 +0200

    Add: Exercise-JavaScript-37/ Promise

--- exercise.js
+++ exercise.js
@@ -1,17 +1,41 @@
-
-
+// FORMAT .then .catch
 const number = 15;
 
-const verify = async () => {
-    try {
-        if (number > 10) {
-            console.log('The constant is greater than 10 : Resolve')
-        } else {
-            console.log('The constant is not greater than 10 : Reject');
-        }
-    } catch (error) {
-        console.log(error);
+function prom() {
+  return new Promise((resolve, reject) => {
+    if (number > 10) {
+      resolve(`The constant is greater than 10 : Resolve`);
+    } else {
+      reject(`The constant is not greater than 10 : reject`);
     }
-};
+  });
+}
 
-verify();
+const verify = prom();
+
+verify
+  .then((risp) => {
+    console.log(risp);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+// FORMAT async await 
+
+// const number = 15;
+
+// const verify = async () => {
+//     try {
+//         if (number > 10) {
+//             console.log('The constant is greater than 10 : Resolve')
+//         } else {
+//             console.log('The constant is not greater than 10 : Reject');
+//         }
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
+
+// verify();
